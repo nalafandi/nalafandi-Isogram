11a8f516b402870cca0d30304560230ad1f7947c
nalafandi <niafandi@gmail.com>
2023-03-06 21:38:36 +0000

    Done!

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Isogram {
     /**
@@ -13,6 +15,14 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        char[] chars =  str.toCharArray();
+        Set<Object> decision = new HashSet<>();
+
+        for (Object o : chars) {
+            if (decision.add(o) == false) {
+                return false;
+            }
+        }
+        return true;
     }
 }
